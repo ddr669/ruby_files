92fbe0942ecf3980805a2832d09a61811bed22d8
ddr669 <dudu.herique.x@gmail.com>
2025-07-19 07:19:34 -0300

    regex with ruby

--- basics/regex_.rb
+++ basics/regex_.rb
@@ -18,3 +18,22 @@ end
 if input_user.match(/h/)
     puts 
 end
+
+# to check if have vowels
+                # like a list of objects
+if "dnthvvwls!" =~ /[aeiou]/
+    puts "Have vowels"
+else
+    puts "Dont have vowels"
+end
+
+# There are different short expressions for specifying character ranges
+#
+#   -    \w is equivalent to [0-9a-zA-Z_]
+#   -   \d is the same as [0-9]
+#   -   \s matches white space
+#   -   \W anything that’s not in [0-9a-zA-Z_]
+#   -   \D anything that’s not a number
+#   -   \S anything that’s not a space
+#  
+#   To match a serie of chars in a string like Number.Number /\d/.\d/
