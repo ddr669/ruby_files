06293da5a57bb3f68e580a6df57f91b431c1cd93
ddr669 <dudu.herique.x@gmail.com>
2025-07-19 02:06:01 -0300

    regex with ruby

---
+++ basics/regex_.rb
@@ -0,0 +1,17 @@
+
+puts "ayucd7845ef" =~ /\d/
+
+# "string" =~ /chr_string/  (chr_string=str) => return true else nil
+
+
+puts "Type something"
+
+input_user = gets
+input_user = input_user.chomp
+
+if input_user =~ /h/
+    puts "Your string have 'h'"
+else
+    puts "Your string dont have 'h'"
+end
+
